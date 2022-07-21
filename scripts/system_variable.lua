d90235b4f89597ac9222d27e5e4a5ca474a2316e
Devin <47056144+ikws4@users.noreply.github.com>
2022-07-21 17:18:13 +0800

    Create system_variable.lua

---
+++ scripts/system_variable.lua
@@ -0,0 +1,6 @@
+-- metadata about this script
+return {
+  name = "System Variable",
+  author = "ikws4",
+  description = "Configing the variables, like phone model, location, default language etc...",
+}
