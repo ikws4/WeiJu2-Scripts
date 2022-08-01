f29c687be4d630fe3e5290c7b7c5305bc6d7c310
Devin <47056144+ikws4@users.noreply.github.com>
2022-08-01 11:31:53 +0800

    Update example.lua

--- scripts/example.lua
+++ scripts/example.lua
@@ -4,7 +4,7 @@ return {
   name = "Example",
   author = "ikws4",
   version = "1.0.0",
-  description "An example script, does nothing"
+  description = "An example script, does nothing"
 }
 @end
 --]]
