9b2af2e7fe7fa8f289d55056340498f61c83fd8d
Devin <47056144+ikws4@users.noreply.github.com>
2022-08-01 11:35:44 +0800

    Update example.lua

--- scripts/example.lua
+++ scripts/example.lua
@@ -1,10 +1,10 @@
 --[[ 
 @metadata
-return {
-  name = "Example",
-  author = "ikws4",
-  version = "1.0.0",
-  description = "An example script, does nothing"
-}
+  return {
+    name = "Example",
+    author = "ikws4",
+    version = "1.0.0",
+    description = "An example script, does nothing"
+  }
 @end
 --]]
