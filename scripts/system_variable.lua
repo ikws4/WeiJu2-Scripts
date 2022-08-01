bec8b05911904ec5634afc98638ce08e2187fb8a
Devin <47056144+ikws4@users.noreply.github.com>
2022-08-01 11:14:44 +0800

    Update system_variable.lua

--- scripts/system_variable.lua
+++ scripts/system_variable.lua
@@ -1,4 +1,10 @@
---- @name "System Variable"
---- @author "ikws4"
---- @version "1.0.0"
---- @description "Configing the variables, like phone model, location, default language etc..."
+--[[ 
+@metadata
+return {
+  name = "System Variable",
+  author = "ikws4",
+  version = "1.0.0",
+  description "Configing the variables, like phone model, location, default language etc..."
+}
+@end
+--]]
