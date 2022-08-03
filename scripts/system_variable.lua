9599126a8157df3815e19c0af4ce64c0d48bcf81
ikws4 <zhipingne@gmail.com>
2022-08-03 15:59:24 +0800

    Update system_variable.lua

--- scripts/system_variable.lua
+++ scripts/system_variable.lua
@@ -9,65 +9,50 @@
 @end
 --]]
 
--- Build Info
-local device = "coral"
-local product = "coral"
-local model = "Pixel 4 XL"
-local brand = "google"
-local android_version = "10"
-
--- Location
--- See https://www.latlong.net/
-local longitude = 31.921279
-local latitude = 620.157480
-
-
-local build_fields = {
-  DEVICE = device,
-  PRODUCT = product,
-  MODEL = model,
-  BRAND = brand,
-  MANUFACTURER = brand,
+local config = {
+	build = {
+		device = "coral",
+		product = "coral",
+		model = "Pixel 4 XL",
+		brand = "google",
+		android_version = "10",
+	},
+	location = { -- See https://www.latlong.net/
+		longitude = 31.921279,
+		latitude = 620.157480,
+	},
 }
 
-for k, v in pairs(build_fields) do
-  xp.set_field {
-    class = "android.os.Build",
-    type = "java.lang.String",
-    field = k,
-    value = v
-  }
-end
+import "android.os.Build"
 
-xp.set_field {
-  class = "android.os.Build$VERSION",
-  type = "java.lang.String",
-  field = "RELEASE",
-  value = android_version,
-}
-
+Build.DEVICE = config.device
+Build.PRODUCT = config.product
+Build.MODEL = config.model
+Build.BRAND = config.brand
+Build.MANUFACTURER = config.brand
+Build.VERSION.RELEASE = config.android_version
 
 local location_classes = {
-  "android.location.Location",        -- Android
-  "com.baidu.location.BDLocation",    -- Baidu
+	"android.location.Location",     -- Android
+	"com.baidu.location.BDLocation", -- Baidu
 }
 
 for _, class in ipairs(location_classes) do
-  xp.hook {
-    class = class,
-    returns = "double",
-    method = "getLongitude",
-    replace = function(this, args)
-      return longitude
-    end,
-  }
+	xp.hook({
+		class = class,
+		returns = "double",
+		method = "getLongitude",
+		replace = function(this, args)
+			return config.location.longitude
+		end,
+	})
 
-  xp.hook {
-    class = class,
-    returns = "double",
-    method = "getLatitude",
-    replace = function(this, args)
-      return latitude
-    end,
-  }
+	xp.hook({
+		class = class,
+		returns = "double",
+		method = "getLatitude",
+		replace = function(this, args)
+			return config.loation.latitude
+		end,
+	})
 end
