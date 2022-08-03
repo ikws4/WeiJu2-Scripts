d0a677ed9458e493d490f893486e5bc260d26fc6
Devin <47056144+ikws4@users.noreply.github.com>
2022-08-03 10:32:56 +0800

    Update system_variable.lua

--- scripts/system_variable.lua
+++ scripts/system_variable.lua
@@ -8,3 +8,45 @@
   }
 @end
 --]]
+
+local device = "coral"
+local product = "coral"
+local model = "Pixel 4 XL"
+local brand = "google"
+local android_version = "10"
+
+xp.set_field {
+  class = "android.os.Build",
+  field = "DEVICE",
+  value = device,
+}
+
+xp.set_field {
+  class = "android.os.Build",
+  field = "PRODUCT",
+  value = product,
+}
+
+xp.set_field {
+  class = "android.os.Build",
+  field = "MODEL",
+  value = model,
+}
+
+xp.set_field {
+  class = "android.os.Build",
+  field = "BRAND",
+  value = brand,
+}
+
+xp.set_field {
+  class = "android.os.Build",
+  field = "MANUFACTURER",
+  value = brand,
+}
+
+xp.set_field {
+  class = "android.os.Build$VERSION",
+  field = "RELEASE",
+  value = android_version,
+}
