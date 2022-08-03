ffc2e54a3dbf100487cf0a9e32eb36368a7d2601
ikws4 <zhipingne@gmail.com>
2022-08-03 16:36:10 +0800

    Update example.lua

--- scripts/example.lua
+++ scripts/example.lua
@@ -11,37 +11,43 @@
 
 -- Easy to import any java class and support for private field, method and constructor access
 local Toast = import("android.widget.Toast")
+local Activity = import("android.app.Activity")
+local TextView = import("android.widget.TextView")
+local Bundle = import("android.os.Bundle")
+local void = import("void")
+local CharSequence = import("java.lang.CharSequence")
+local BufferType = import("android.widget.TextView$BufferType")
+local boolean = import("boolean")
+local int = import("int")
 
 -- Make a toast after activity created
-xp.hook {
-  class = "android.app.Activity",
-  returns = "void",
-  method = "onCreate",
-  params = {
-    "android.os.Bundle" -- Bundle
-  },
-  after = function(this, args)
-    Toast:makeText(this, "Hello, WeiJu2!", Toast.LENGTH_SHORT):show()
-    --              ^
-    -- Note: `this` is the Activity instance
-  end
-}
+xp.hook({
+	class = Activity,
+	returns = void,
+	method = "onCreate",
+	params = { Bundle },
+	after = function(this, args)
+		Toast:makeText(this, "Hello, WeiJu2!", Toast.LENGTH_SHORT):show()
+		--              ^
+		-- Note: `this` is the Activity instance
+	end,
+})
 
 -- Change all the text to "WeiJu2"
-xp.hook {
-  class = "android.widget.TextView",
-  returns = "void",
-  method = "setText",
-  params = {
-    "java.lang.CharSequence", -- text
-    "android.widget.TextView$BufferType", -- type
-    "boolean", -- nofityBefore
-    "int", -- oldLen
-  },
-  before = function(self, args)
-    -- Reset `text` value
-    args[1] = "WeiJu2"
-    --   ^
-    -- Note: In lua, index is 1 based
-  end
-}
+xp.hook({
+	class = TextView,
+	returns = void,
+	method = "setText",
+	params = {
+		CharSequence, -- text
+		BufferType, -- type
+    boolean, -- nofityBefore
+    int, -- oldLen
+	},
+	before = function(self, args)
+		-- Reset `text` value
+		args[1] = "WeiJu2"
+		--   ^
+		-- Note: In lua, index is 1 based
+	end,
+})
