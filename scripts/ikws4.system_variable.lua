eb981362dc04e0ab7faf471e488ade99fce40e00
ikws4 <zhipingne@gmail.com>
2022-08-15 19:19:19 +0800

    Update ikws4.system_variable
    Add dpi and language config

--- scripts/ikws4.system_variable.lua
+++ scripts/ikws4.system_variable.lua
@@ -1,13 +1,32 @@
---[[ 
+--[=[
 @metadata
   return {
     name = "system_variable",
     author = "ikws4",
-    version = "1.0.1",
-    description = "Configing the variables, like phone model, location, default language etc..."
+    version = "1.1.1",
+    description = "Configing the variables, like phone model, location, dpi etc...",
+    example = [[
+      require("ikws4.system_variable").setup {
+        build = {
+          device = "coral",
+          product = "coral",
+          model = "Pixel 4 XL",
+          brand = "google",
+          android_version = "13",
+        },
+        location = {
+          longitude = 31.921279,
+          latitude = 620.157480,
+        },
+        display = {
+          dpi = 320,
+          language = "en-US",
+        },
+      }
+    ]]
   }
 @end
---]]
+--]=]
 
 local config = {
   build = {
@@ -21,6 +40,11 @@ local config = {
     -- See https://www.latlong.net/
     longitude = nil,
     latitude = nil,
+  },
+  display = {
+    dpi = nil,
+    -- See https://www.fincher.org/Utilities/CountryLanguageList.shtml
+    language = nil,
   },
 }
 
@@ -69,6 +93,44 @@ M.setup = function(opts)
         }
       end
     end
+  end
+
+  if config.display.dpi or config.display.language then
+    local ContextWrapper = import("android.content.ContextWrapper")
+    local Configuration = import("android.content.res.Configuration")
+    local Context = import("android.content.Context")
+
+    hook {
+      class = ContextWrapper,
+      returns = void,
+      method = "attachBaseContext",
+      params = {
+        Context,
+      },
+      before = function(this, params)
+        local context = params[1]
+
+        -- Make a copy of the configuration, and then modify the dpi
+        local new_configuration = Configuration(context:getResources():getConfiguration())
+
+        if config.display.dpi then
+          new_configuration.densityDpi = config.display.dpi
+        end
+
+        -- Change the language
+        if config.display.language then
+          local String = import("java.lang.String")
+          local Locale = import("java.util.Locale")
+          
+          local country_city = config.display.language:split("-")
+          local locale = Locale(country_city[1], country_city[2])
+          Locale:setDefault(locale)
+          new_configuration:setLocale(locale)
+        end
+
+        params[1] = context:createConfigurationContext(new_configuration)
+      end,
+    }
   end
 end
 
